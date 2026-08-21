c2fdd7a01cb6e293efd820716f60adb02a56cf57
Alice Vitoria <alice.vitoria.silva28012004@gmail.com>
2026-08-20 23:09:31 -0300

    Remove commented-out code for menu toggle

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// Função para o menu-toggle (se aplicável)
-//const menuToggle = document.querySelector(".menu-toggle");
-//const nav = document.querySelector("nav");
-//menuToggle.addEventListener("click", () => {
-//nav.classList.toggle("open");
-//});
-
-// script.js
 const menuToggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.list-menu');
 
